2089b9b839371d5bd28819f1d37507932ed323ad
Anurag Salian <126774255+flutterdevanurag@users.noreply.github.com>
2024-04-25 18:49:30 +0530

    init

---
+++ src/main/java/io/delta/expensetrackerserver/ExpenseTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package io.delta.expensetrackerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/delta/expensetrackerserver/ExpenseTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.delta.expensetrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
